6e2ff28d8594c9fa64d2029005cc9a31c0facc7e
Michael Iodice <miodice3@gmail.com>
2020-11-11 17:39:11 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,56 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    #test linking pages
+    erb :'articles/index'
   end
+
+  get '/articles/new' do
+    erb :'articles/new'
+  end
+
+  post '/articles' do
+    article = Article.create(params)
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles' do
+    #should show all of the articles
+    @articles = Article.all
+    erb :'articles/index'
+  end
+
+  get '/articles/:id' do #SHOW
+    #displays an individual article
+    @article = Article.find_by(id: params[:id])
+
+    if @article
+      erb :'articles/show'
+    else
+      "Whoops! that Article does not exist."
+    end
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(id: params[:id])
+    #binding.pry
+    erb :'articles/edit'
+  end
+
+  patch '/articles/:id' do
+
+    article = Article.find_by_id(params[:id])
+    article.update(params[:article])
+
+    redirect "/articles/#{article.id}"
+  end
+
+  delete '/articles/:id' do #DESTROY
+    #binding.pry
+    article = Article.find_by_id(params[:id])
+    article.destroy
+
+    redirect "/articles"
+  end
+
 end
